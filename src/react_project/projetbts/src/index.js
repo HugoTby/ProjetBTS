d1d6815ea66af3cb61e0f606668032991dcdaf24
HugoTby <hugotabarylapro@gmail.com>
2024-03-15 16:08:46 +0100

    commit

--- src/react_project/projetbts/src/index.js
+++ src/react_project/projetbts/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Login_component from './components/login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login_component />
   </React.StrictMode>
 );
 
