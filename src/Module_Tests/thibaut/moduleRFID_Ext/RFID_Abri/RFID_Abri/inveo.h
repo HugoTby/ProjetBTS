2a95eb716664b96991a2d031187f0064ce398a7b
= <=>
2024-05-22 08:11:59 +0200

    création .h de la classe inveo

--- src/Module_Tests/thibaut/moduleRFID_Ext/RFID_Abri/RFID_Abri/inveo.h
+++ src/Module_Tests/thibaut/moduleRFID_Ext/RFID_Abri/RFID_Abri/inveo.h
@@ -4,11 +4,28 @@
 class inveo :
     public QModbusTcpClient
 {
-
-private :
+    Q_OBJECT
     QTimer* timer;
-public :
+    const QString defaultData;
+    QString lastDataString;
 
-    
+private:
+
+public:
+    inveo(QString readerIP, int port = 502, QObject* parent = nullptr);
+    virtual ~inveo();
+    virtual void connectToHost();
+    void emitAcceptSound();
+    void emitRejectSound();
+
+private slots:
+    void receiveMultipleHoldingRegistersSentence(quint16 startAdress, QVector <quint16> values);
+    void onConnected();
+    void onDisconnected();
+    void readDataSlot();
+signals:
+    void cardScanned(QString cardData);
+    void linkEstablished();
+    void linkLost();
 };
 
